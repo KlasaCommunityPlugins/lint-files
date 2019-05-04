6a600b3e8d75ab5e76618803ed2952acf9c4fafd
Vlad Frangu <kingdgrizzle@gmail.com>
2019-05-04 11:29:22 +0300

    src: Fix parser issues

--- eslint-ts.js
+++ eslint-ts.js
@@ -1,7 +1,7 @@
 module.exports = {
-	"parser": "@typescript-eslint/parser",
 	"plugins": ["@typescript-eslint"],
 	"parserOptions": {
+		"parser": "@typescript-eslint/parser",
 		"ecmaVersion": 2019,
 		"project": "./tsconfig.json",
 		"sourceType": "module",
